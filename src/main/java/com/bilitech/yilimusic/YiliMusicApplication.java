a93afb1eb11acef68de5197c64ce9309670af5a2
xyb1314 <1025071499@qq.com>
2022-01-07 11:15:15 +0800

    first commit the original version from the coder

---
+++ src/main/java/com/bilitech/yilimusic/YiliMusicApplication.java
@@ -0,0 +1,14 @@
+package com.bilitech.yilimusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class YiliMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YiliMusicApplication.class, args);
+    }
+
+}
