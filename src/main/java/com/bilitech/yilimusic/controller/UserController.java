e2db5096673513c7006746ef78b53318ac150014
xyb1314 <1025071499@qq.com>
2022-01-09 17:56:39 +0800

    solve the crossOrigin Problems

--- src/main/java/com/bilitech/yilimusic/controller/UserController.java
+++ src/main/java/com/bilitech/yilimusic/controller/UserController.java
@@ -4,6 +4,7 @@ import com.bilitech.yilimusic.mapper.UserMapper;
 import com.bilitech.yilimusic.service.UserService;
 import com.bilitech.yilimusic.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/users")
+
 public class UserController {
 
     UserService userService;
@@ -25,7 +27,7 @@ public class UserController {
                 .map(userMapper::toVo).collect(Collectors.toList());
     }
 
-
+    @CrossOrigin
     @GetMapping("/hello")
     public String sayhello(){
 
